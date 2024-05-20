a17aef8958e358502ecd6d7b2a01d988da31d7bf
ZHEHG,YU-HENG <48355136+kenchick2S@users.noreply.github.com>
2024-05-20 09:44:34 +0800

    Create context.js

---
+++ GetCsv/client/context.js
@@ -0,0 +1,8 @@
+import { createContext } from "react"
+
+export const FunctionDialogContext = createContext({
+    analysis_state: null,
+    analysis_setter: null,
+    query_state: null,
+    query_setter: null
+});
