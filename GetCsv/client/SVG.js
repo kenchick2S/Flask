6979e11a87b6bf2e73a713b1d999be726be27be9
ZHEHG,YU-HENG <48355136+kenchick2S@users.noreply.github.com>
2024-05-20 09:40:34 +0800

    Create SVG.js

---
+++ GetCsv/client/SVG.js
@@ -0,0 +1,57 @@
+
+function PaperClipSVG(){
+	return(
+		<svg 
+			xmlns="http://www.w3.org/2000/svg" 
+			xlink="http://www.w3.org/1999/xlink" 
+			version="1.1" viewBox="0 0 256 352 " 
+		>
+			<defs>
+			</defs>
+
+			<g style={{stroke: 'none', strokeWidth: 0, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'none', fillRule: 'nonzero', opacity: 0.5}} transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+				<path d="M 26.827 89.998 c -5.832 0 -11.664 -2.22 -16.104 -6.66 c -8.88 -8.88 -8.88 -23.329 0 -32.209 L 56.932 4.921 c 6.563 -6.564 17.244 -6.563 23.805 0 c 6.563 6.563 6.563 17.241 0 23.804 L 37.658 71.804 c -4.245 4.247 -11.155 4.247 -15.401 0 c -4.246 -4.246 -4.246 -11.155 0 -15.401 l 39.3 -39.3 c 0.781 -0.781 2.047 -0.781 2.829 0 c 0.781 0.781 0.781 2.048 0 2.829 l -39.3 39.3 c -2.686 2.686 -2.686 7.057 0 9.743 c 2.687 2.687 7.058 2.685 9.743 0 l 43.079 -43.079 c 5.003 -5.003 5.003 -13.144 0 -18.147 c -5.004 -5.004 -13.145 -5.003 -18.147 0 L 13.552 53.959 c -7.319 7.32 -7.319 19.231 0 26.551 c 7.322 7.32 19.232 7.319 26.551 0 L 82.533 38.08 c 0.781 -0.781 2.047 -0.781 2.829 0 c 0.781 0.781 0.781 2.048 0 2.829 L 42.931 83.339 C 38.491 87.778 32.659 89.998 26.827 89.998 z" style={{stroke: 'none', strokeWidth: 1, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'rgb(0,0,0)', fillRule: 'nonzero', opacity: 0.5}} transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round"/>
+			</g>
+		</svg>
+	)
+}
+
+function ArrowSVG(){
+
+	return(
+		<svg
+			className="arrow"
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+			/>
+		</svg>
+	)
+}
+
+function CopilotSVG(){
+	return(
+		<svg className="regularSVG" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 17A1.5 1.5 0 0 1 3 15.65V4.5c0-.78.6-1.42 1.36-1.5H9c.78 0 1.42.6 1.5 1.36v.4l2.19-2.27a1.5 1.5 0 0 1 2-.14l.12.1 2.76 2.72c.55.55.6 1.42.11 2.02l-.1.11-2.31 2.2h.23c.78 0 1.42.6 1.5 1.36v4.64c0 .78-.6 1.42-1.36 1.5H4.5Zm5-6.5H4v5c0 .22.14.4.33.47l.08.02.09.01h5v-5.5Zm6 0h-5V16h5a.5.5 0 0 0 .5-.4V11a.5.5 0 0 0-.41-.5h-.09Zm-5-2.79V9.5h1.79L10.5 7.71ZM9 4.01H4.5a.5.5 0 0 0-.5.4v5.1h5.5v-5a.5.5 0 0 0-.33-.48l-.08-.02H9Zm5.12-.84a.5.5 0 0 0-.64-.05l-.07.06-2.62 2.71a.5.5 0 0 0-.05.63l.06.07 2.61 2.62c.17.17.43.2.62.07l.08-.06 2.76-2.63a.5.5 0 0 0 .05-.64l-.05-.06-2.75-2.72Z" fill="currentColor"></path></svg>
+	)
+}
+
+function CopilotSVG2(){
+	return(
+		<svg className="hoverSVG" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 17A1.5 1.5 0 0 1 3 15.65V4.5c0-.78.6-1.42 1.36-1.5H9c.78 0 1.42.6 1.5 1.36v.4l2.19-2.27a1.5 1.5 0 0 1 2-.14l.12.1 2.76 2.72c.55.55.6 1.42.11 2.02l-.1.11-2.31 2.2h.23c.78 0 1.42.6 1.5 1.36v4.64c0 .78-.6 1.42-1.36 1.5H4.5Zm5-6.5H4v5c0 .22.14.4.33.47l.08.02.09.01h5v-5.5Zm6 0h-5V16h5a.5.5 0 0 0 .5-.4V11a.5.5 0 0 0-.41-.5h-.09Zm-5-2.79V9.5h1.79L10.5 7.71ZM9 4.01H4.5a.5.5 0 0 0-.5.4v5.1h5.5v-5a.5.5 0 0 0-.33-.48l-.08-.02H9Z" fill="currentColor"></path></svg>
+	)
+}
+
+function CopilotClipSVG(){
+	return (
+		<svg fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="m5.28 10.6 5.3-5.3a2.75 2.75 0 1 1 3.9 3.9L8.1 15.55a1.25 1.25 0 1 1-1.77-1.77L12 8.13a.75.75 0 0 0-1.06-1.06l-5.66 5.66a2.75 2.75 0 0 0 3.89 3.89l6.36-6.36a4.25 4.25 0 0 0-6-6.02l-5.31 5.3a.75.75 0 0 0 1.06 1.07Z" fill="currentColor"></path></svg>
+	)
+}
+
+export {ArrowSVG, PaperClipSVG, CopilotSVG, CopilotClipSVG, CopilotSVG2};
